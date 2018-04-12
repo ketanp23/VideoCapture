cbdf6dcd21c5811552e717c819dcdcead551bd26
ketanp23 <ketanp23@users.noreply.github.com>
2018-04-12 17:53:24 +0530

    Update capfromfile.py

--- capfromfile.py
+++ capfromfile.py
@@ -1,20 +1,15 @@
 
 #Import Numpy & OpenCV (OpenCV :- V.3.4.0)
-import numpy as np
 import cv2
 
-
-
 cap=cv2.VideoCapture('MOV00023.3gp') #Set Video File name
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320) #Set Resolution Width
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240) #Set Resolution Height
 cap.set(cv2.CAP_PROP_FPS, 10) #Set Frames Per Second Rate
 
 success,image = cap.read()
 count = 0
 success = True
 while success:
-  cv2.imwrite("frame%d.jpg" % count, image) #Save frame as JPEG file
+  cv2.imwrite("data/frame%d.jpg" % count, cv2.resize(image, (320, 240)))  #Save frame as JPEG file as per resolution
   success,image = cap.read()
   print('Read a new frame: ', success)
   count += 1
